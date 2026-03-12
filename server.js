d824b3c8e7c08d2523c9865360a5541399843b6f
Andre Abreu <andre.abreu@gmail.com>
2026-03-12 20:39:19 -0300

    fix: use PORT env variable for App Platform compatibility

--- server.js
+++ server.js
@@ -277,7 +277,7 @@ function calculateFlightSummary(fixes, igcData) {
   };
 }
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`IGC Interpreter running at http://localhost:${PORT}`);
 });
